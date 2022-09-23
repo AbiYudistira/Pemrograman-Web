32c3741efb1da6d322268bd36b5ef5d49f7f898f
AbiYudistira <abai86103@gmail.com>
2022-09-23 11:09:37 +0700

    Push1

---
+++ pertemuan1/Latihan_INCLUDE/content_include.php
@@ -0,0 +1,7 @@
+<div class="content">
+    <br/>
+    home / About Us
+    <h1>Tentang saya</h1>
+    <p>Nama saya Abi, dari Smk Negeri 5 Surakarta.</p>
+    
+</div>
---
+++ pertemuan1/Latihan_INCLUDE/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan Mandiri</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Abi Yudistira</h1>
+        <i>Latihan include & require</i>
+        <hr/>
+    </div>
+
+    <div class="content">
+        <br/>
+        home / About Us
+        <h1>Tentang saya</h1>
+        <p>Nama saya Abi, dari SMKN 5 Surakarta.</p>
+    </div>
+
+    <div class="footer">
+        <hr/>
+        include & require
+    </div>
+</body>
+</html>
