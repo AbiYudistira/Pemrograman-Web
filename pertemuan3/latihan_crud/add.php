01d1fe98c0e9014ed23008f645c7c288013bfd4f
AbiYudistira <abai86103@gmail.com>
2022-11-16 11:20:14 +0700

    pertemuan3

--- pertemuan3/latihan_crud/add.php
+++ pertemuan3/latihan_crud/add.php
@@ -2,52 +2,32 @@
 <head>
     <title>Add Customer</title>
 </head>
-
+ 
 <body>
     <a href="index.php">Go to Home</a>
     <br/><br/>
-
-    <form action="add.process.php" method="post" name="form1">
+ 
+    <form action="add_process.php" method="post" name="form1">
         <table width="25%" border="0">
-            <tr>
+            <tr> 
                 <td>Name</td>
                 <td><input type="text" name="name"></td>
             </tr>
-            <tr>
+            <tr> 
                 <td>Email</td>
                 <td><input type="text" name="email"></td>
             </tr>
-            <tr>
+            <tr> 
                 <td>Phone</td>
                 <td><input type="text" name="phone"></td>
             </tr>
-            <tr>
+            <tr> 
                 <td></td>
                 <td><input type="submit" name="Submit" value="Add"></td>
             </tr>
         </table>
     </form>
-
+    
+    
 </body>
 </html>
-
-<?php
-//Cek jika form telah di submit, kemudian memasukkan data dari form ke customers tabel.
-if(isset($_POST['Submit'])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    date_default_timezone_set('Asia/Jakarta');
-    $created = date("Y-m-d H:i:s");
-
-    //Membuat koneksi ke database menggunakan file config.php dengan include_once
-    include_once("config.php");
-
-    //Memasukkan data customers ke tabel customers
-    // $conn_db berasal dari config.php
-    $result = mysqli_query($conn_db, "INSERT INTO customers(name,email,phone,created)VALUES('$name','$email','$phone','$created')");
-
-    //Menampilkan pesan ketika customers telah ditambah
-    echo "Customer ". $name ." added succesfully. <a href='index.php'>View Customers</a>";
-}
-?>
--- pertemuan3/latihan_crud/index.php
+++ pertemuan3/latihan_crud/index.php
@@ -1,48 +1,48 @@
 <?php
-// Membuat koneksi database melalui file confic.php dengan include_once
+// Membuat koneksi database melalui file config.php dengan include_once
 include_once("config.php");
 // Mengambil semua data customers dari database
 $query = "SELECT * FROM customers ORDER BY id DESC";
 $result = mysqli_query($conn_db, $query);
 
 ?>
-
+ 
 <html>
-<head>
-    <title>Homepage</title>
+<head>    
+    <title>Home</title>
 </head>
-
+ 
 <body>
 <a href="add.php">Add New Customers</a><br/><br/>
-    <table width='80%' border=1></table>
+    <table width='80%' border=1>
         <thead>
             <tr>
-                <th>Name</th>
-                <th>Phone</th>
-                <th>Email</th>
-                <th>Join Date</th>
-                <th>Last Updated</th>
+                <th>Name</th> 
+                <th>Phone</th> 
+                <th>Email</th> 
+                <th>Join Date</th> 
+                <th>Last Updated</th> 
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            <!-- Fetch data customers dengan array associative -->
+            <!-- Fetch data customer dengan array associative -->
             <?php if($result->num_rows > 0): ?>
-                <?php while($customers_data = mysqli_fetch_array($result)) : ?>
+                <?php while($customer_data = mysqli_fetch_array($result)) : ?>        
+                    <tr>
+                        <td> <?php echo $customer_data['name']?> </td>
+                        <td> <?php echo $customer_data['phone']?> </td>
+                        <td> <?php echo $customer_data['email']?> </td> 
+                        <td> <?= $customer_data['created']?> </td>    
+                        <td> <?= $customer_data['updated']?> </td>
+                        <td>
+                            <a href='edit.php?id=<?=$customer_data["id"]?>'>Edit</a> | <a href='delete.php?id=<?=$customer_data["id"]?>'>Delete</a>
+                        </td>
+                    </tr>        
+                <?php endwhile; ?>
+            <?php else : ?>
                 <tr>
-                    <td> <?php echo $customers_data['name']?> </td>
-                    <td> <?php echo $customers_data['phone']?> </td>
-                    <td> <?php echo $customers_data['email']?> </td>
-                    <td> <?= $customers_data['created']?> </td>
-                    <td> <?= $customers_data['updated']?> </td>
-                    <td>
-                        <a href='edit.php?id=<?=$customers_data["id"]?>'>Edit</a> | <a href='delete.php?id=<?=$customers_data["id"]?>'>Delete</a>
-                    </td>    
-                </tr>
-            <?php endwhile; ?>
-        <?php else : ?>
-                <tr>
-                    <td colspan="6" rowspan="1" headers="" align="center">Data customers tidak ditemukan!</td>
+                    <td colspan="6" rowspan="1" headers="" align="center">Data customer tidak ditemukan!</td>
                 </tr>
             <?php endif; ?>
 
@@ -52,11 +52,11 @@ $result = mysqli_query($conn_db, $query);
 
     <?php if(isset($_GET["message"])){
         $message = $_GET["message"];
-        echo "<script type='text/javascript'>alert('$mesaage');";
-        echo "window.location='index.php'<script";
-    }
+        echo "<script type='text/javascript'>alert('$message');";
+        echo "window.location='index.php'</script>";
+    }    
     ?>
-
+        
     
 </body>
 </html>
